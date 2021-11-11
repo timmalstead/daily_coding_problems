a9f0ce61364fcfa684f26f9fdb75df14f6e2b097
timmalstead <timmalstead@gmail.com>
2021-11-10 20:11:05 -0800

    two appear once

--- dailyCoding3.ts
+++ dailyCoding3.ts
@@ -1007,7 +1007,24 @@ const minNumOfCoins = (n: number, coins: number[]): number => {
 //   return shortestLength;
 // };
 
-log(minNumOfCoins(50, [1, 5, 10, 25]));
+// log(minNumOfCoins(50, [1, 5, 10, 25]));
+
+//#endregion
+
+//#region
+
+// Given an array of integers in which two elements appear exactly once and all other elements appear exactly twice, find the two elements that appear only once.
+
+// For example, given the array [2, 4, 6, 8, 10, 2, 6, 10], return 4 and 8. The order does not matter.
+
+// Follow-up: Can you do this in linear time and constant space?
+
+const twoAppearOnce = (numArr: number[]): [number, number] | any =>
+  numArr.filter(
+    (num, _, arr): boolean => arr.indexOf(num) === arr.lastIndexOf(num)
+  );
+
+log(twoAppearOnce([2, 4, 6, 8, 10, 2, 6, 10]));
 
 //#endregion
 
