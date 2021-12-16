c88ca54ef3cad1b53a82289cf2cbf79630531f21
timmalstead <timmalstead@gmail.com>
2021-12-16 08:57:45 -0800

    one to one mapping problem. pretty easy unless there is something that i'm not seeing

--- dailyCoding4.ts
+++ dailyCoding4.ts
@@ -278,7 +278,20 @@ const findConcatWordsIndices = (s: string, words: string[]): number[] => {
   return indices;
 };
 
-log(findConcatWordsIndices("dogcatcatcodecatdog", ["cat", "dog"]));
+// log(findConcatWordsIndices("dogcatcatcodecatdog", ["cat", "dog"]));
+
+//#endregion
+
+//#region find one to one mapping in strings
+
+// Determine whether there exists a one-to-one character mapping from one string s1 to another s2.
+// For example, given s1 = abc and s2 = bcd, return true since we can map a to b, b to c, and c to d.
+// Given s1 = foo and s2 = bar, return false since the o cannot map to two characters.
+
+const hasOneToOneCharMapping = (s1: string, s2: string): boolean =>
+  new Set<string>(s1).size === new Set<string>(s2).size;
+
+log(hasOneToOneCharMapping("abc", "bcd"), hasOneToOneCharMapping("foo", "bar"));
 
 //#endregion
 
