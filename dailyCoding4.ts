6e2b991abef3e0e0ee18530e6b2f4857fc4b4b38
timmalstead <timmalstead@gmail.com>
2022-01-19 11:23:14 -0800

    wrote functions exploring collatz sequence

--- dailyCoding4.ts
+++ dailyCoding4.ts
@@ -841,6 +841,31 @@ const findPermutations = (str: string): Set<string> | string | null => {
 
 //#endregion
 
+//#region collatz sequence
+
+// A Collatz sequence in mathematics can be defined as follows. Starting with any positive integer:
+
+// if n is even, the next number in the sequence is n / 2
+// if n is odd, the next number in the sequence is 3n + 1
+// It is conjectured that every such sequence eventually reaches the number 1. Test this conjecture.
+
+// Bonus: What input n <= 1000000 gives the longest sequence?
+
+const isEven = (n: number): boolean => n % 2 === 0;
+
+const collatzSeq = (n: number): void => {
+  while (n > 1) {
+    log(n);
+    if (isEven(n)) n /= 2;
+    else n = n * 3 + 1;
+  }
+  log(n);
+};
+
+collatzSeq(1000);
+
+//#endregion
+
 //#region
 
 //#endregion
